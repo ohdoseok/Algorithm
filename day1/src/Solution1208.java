ed56cefca4476e8ffc93bdbd5be5768c7aa67bd5
ohdoseok <pkqwe37@gmail.com>
2022-02-06 23:36:11 +0900

    SWEA1208

--- day1/src/Solution1208.java
+++ day1/src/Solution1208.java
@@ -1,6 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class Solution1208 {
 	public static void main(String[] args) {
+		  	Scanner sc = new Scanner(System.in);
+		  	
+		  	int[] result = new int[11];
+		  	for (int i = 1; i < 11; i++) {
+		  		int Testcase = sc.nextInt();
+		  		ArrayList<Integer> arr = new ArrayList<>();
+				for (int j = 0; j < 100; j++) {
+					arr.add(sc.nextInt());
+				}
+				for (int j = 0; j < Testcase; j++) {
+					Collections.sort(arr);
+					arr.set(99,Collections.max(arr)-1);
+					arr.set(0, Collections.min(arr)+1);
+				}
+				result[i]=Collections.max(arr)-Collections.min(arr);
+			}
+		  	for (int j = 1; j < result.length; j++) {
+		  		System.out.printf("#%d %d%n",j,result[j]);
+			}
 		  	
 	}
 }
+
