b797ef0e37f405b3509e7a0ace1e374b8fc871c0
ohdoseok <pkqwe37@gmail.com>
2022-02-08 21:42:25 +0900

    계산기 구현 .미완성

---
+++ day1/src/Calculation.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Calculation {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Stack<Character> cal = new Stack<>();
+		char[] arr = br.readLine().toCharArray();
+		char[] arrsort = new char[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			
+		}
+	}
+}
+    
