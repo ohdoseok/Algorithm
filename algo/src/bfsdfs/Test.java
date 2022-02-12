f2da3ad59cac8c96bca797414d86fc47ee368baa
ohdoseok <pkqwe37@gmail.com>
2022-02-12 20:09:49 +0900

    dfs,bfs Test

--- algo/src/bfsdfs/Test.java
+++ algo/src/bfsdfs/Test.java
@@ -6,11 +6,14 @@ public class Test {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int size = sc.nextInt();
-		BFS makebfs = new BFS(size);
+//		BFS makebfs = new BFS(size);
+		DFS makedfs = new DFS(size);
 		for (int i = 0; i < size; i++) {
-			makebfs.add((char)('A'+i));
+//			makebfs.add((char)('A'+i));
+			makedfs.add((char)('A'+i));
+			
 		}
-		makebfs.bfs();
-		
+//		makebfs.bfs();
+		makedfs.dfs(1);
 	}
 }
