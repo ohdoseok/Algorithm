1d139eed72e497f28edfd3c13d7395d688d17232
ohdoseok <pkqwe37@gmail.com>
2022-02-23 12:35:02 +0900

    수정

--- algo/src/APractice.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
-//급하게 코드 테스트나 간단하게 구현해보고 지우는 class
-public class APractice {
-	public static void main(String[] args) {
-		int a = Integer.MAX_VALUE;
-	}
-}
