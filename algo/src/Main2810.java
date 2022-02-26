da6537eeee21148c92061fdaabaf8a6b1bea7ded
ohdoseok <pkqwe37@gmail.com>
2022-02-26 14:47:31 +0900

    백준2810.컵홀더 //간단한 규칙

--- algo/src/Main2810.java
+++ algo/src/Main2810.java
@@ -1,37 +1,35 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main2810 {
-	static int N;
-	static int SUM;
-	static int res = 0;
-	static int[] arr;
-	static int[] check = new int[3];
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		SUM = sc.nextInt();
-		arr = new int[N];
-		for (int i = 0; i < N; i++) {
-			arr[i] = sc.nextInt();
-		}
-		com(0,0);
-		System.out.println(res);
-	}
-	
-	static void com(int cnt, int start) {
-		if (cnt==3) {
-			int sum = 0;
-			for (int i = 0; i < 3; i++) {
-				sum+=check[i];
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		char[] arr = new char[T];
+		arr = br.readLine().toCharArray();
+		boolean check = false;
+		int res = 0;
+		for (int i = 0; i < T; i++) {
+			if (arr[i]=='S') {
+				res++;
+			}else if(arr[i]=='L') {
+				if (!check) {
+					res++;
+					check = true;
+				}else if(check) {
+					check=false;
+				}
 			}
-			if (sum>res&&sum<=SUM) {
-				res = sum;
-			}
-			return;
 		}
-		for (int i = start; i < N; i++) {
-			check[cnt] =  arr[i];
-			com(cnt+1,i+1);
+		res++;
+		if (res>=T) {
+			System.out.println(T);
+		}else if(res<T) {
+			System.out.println(res);
 		}
+
+
+
 	}
 }
