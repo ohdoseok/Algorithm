844bdb24483a97de5a30cb6faaf0e0976fd39b61
ohdoseok <pkqwe37@gmail.com>
2022-03-11 00:11:34 +0900

    string builder해서 syso 하는게 매번syso하는거보다 빠르댄다

--- algo/src/Main2751.java
+++ algo/src/Main2751.java
@@ -1,9 +1,21 @@
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main2751 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
+		StringBuilder sb = new StringBuilder();
+		ArrayList<Integer>list = new ArrayList<>();
+		for (int i = 0; i < T; i++) {
+			list.add(sc.nextInt());
+		}
+		Collections.sort(list);
+		for(int a : list) {
+			sb.append(a);
+		}
+		System.out.println(sb);
 	}
 }
